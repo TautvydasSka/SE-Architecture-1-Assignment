96650146e5898adaa7dcd83b24e29d1a526c8c91
TautvydasSka <skardzius.tautvydas@gmail.com>
2020-09-22 00:10:30 +0300

    Frontend work: Work in progress

--- Books/API/ClientApp/src/app/home/home.component.ts
+++ Books/API/ClientApp/src/app/home/home.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit, Inject, ViewChild, ElementRef } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Book } from '../models/book';
+import { FormControl, FormGroup } from '@angular/forms';
 
 declare var $: any;
 
@@ -14,6 +15,10 @@ export class HomeComponent implements OnInit {
   private baseUrl: string;
   public books: Book[];
 
+  public isEdit: boolean;
+
+  private bookForm: FormGroup;
+
   @ViewChild('modal', null) modal: ElementRef;
 
   constructor(http: HttpClient, @Inject('BASE_URL') baseUrl: string) {
@@ -23,16 +28,63 @@ export class HomeComponent implements OnInit {
 
   ngOnInit() {
     this.getBooks();
+
+    this.bookForm = new FormGroup({
+      isbn: new FormControl(''),
+      author: new FormControl(''),
+      title: new FormControl(''),
+      pageCount: new FormControl('')
+    });
   }
 
   private getBooks(): void {
     this.http.get<Book[]>(this.baseUrl + 'api/books').subscribe(result => {
       this.books = result;
-      console.log(this.books);
     }, error => console.error(error));
   }
-  public showModal(isEdit: boolean): void {
-    console.log("clicked");
-    $('#myModal').modal('show');
+
+  public showModal(isEdit: boolean, editedBook: Book = null): void {
+    this.isEdit = isEdit;
+    $('#exampleModal').modal();
+
+    if (editedBook) {
+      this.bookForm.setValue(editedBook);
+    } else {
+      this.bookForm.reset();
+    }
+  }
+
+  public submit(): void {
+    const book: Book = {
+      isbn: this.bookForm.get('isbn').value,
+      author: this.bookForm.get('author').value,
+      title: this.bookForm.get('title').value,
+      pageCount: parseInt(this.bookForm.get('pageCount').value),
+    };
+
+    if (this.isEdit) {
+      this.http.put<object>(this.baseUrl + 'api/books', book).subscribe(result => {
+        console.log(result);
+        this.getBooks();
+      }, error => console.error(error));
+    } else {
+      this.http.post<object>(this.baseUrl + 'api/books', book).subscribe(result => {
+        console.log(result);
+        this.getBooks();
+      }, error => console.error(error));
+    }
+
+    $('#exampleModal').modal('hide')
+  }
+
+  public delete(isbn: string): void {
+    this.http.delete<object>(this.baseUrl + `api/books/${isbn}`).subscribe(result => {
+      console.log(result);
+      this.getBooks();
+    }, error => console.error(error));
+  }
+
+  private handleError(): void {
+
   }
 }
